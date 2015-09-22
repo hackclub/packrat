3d3fa59ef05465a9b92c1c56209d83785fbdae54
Max Wofford <max@hackedu.us>
2015-09-22 00:08:39 -0700

    Add clubs and development club_member to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+clubs = ['Armijo High School',
+         'Cherry Hill High School East',
+         'College Park High School',
+         'Columbia Secondary School',
+         'Elizabeth High School',
+         'Glenaeon Rudolf Steiner School',
+         'Lowell High School',
+         'Project Cipher',
+         'Radnor High School',
+         'Ravenwood High School',
+         'San Leandro High School',
+         'South Brunswick High School',
+         'USC Hybrid High School']
+
+for club in clubs
+  unless Club.find_by(name: club)
+    Club.create(name: club)
+  end
+end
+
+if Rails.env.development?
+  unless Club.find_by(name: 'Foobar High School')
+    Club.create(name: 'Foobar High School')
+  end
+  unless ClubMember.find_by(email: 'foo@bar.com')
+    ClubMember.create(email: 'foo@bar.com', password: 'password', club_id: Club.find_by(name: 'Foobar High School').id)
+  end
+end
