0bb315d1653edddbf3349e782a9da4cfe47ec126
Zach Latta <zach@zachlatta.com>
2015-09-24 13:51:41 -0700

    Refactor and make chart deltas work

--- app/helpers/clubs_helper.rb
+++ app/helpers/clubs_helper.rb
@@ -1,6 +1,6 @@
 module ClubsHelper
-  def club_last_feedback_count(club)
-    club.last_feedback_count || 'N/A'
+  def club_last_attendee_count(club)
+    club.meetings.last.attendee_count || 'N/A'
   end
 
   def club_last_meeting_date(club)
@@ -35,5 +35,19 @@ module ClubsHelper
         data: club.meetings.map { |m| m.average_rating }
       }
     ]
+  end
+
+  def club_meetings_attendee_count_percentage_delta(club)
+    last_meeting = club.meetings[-1]
+    comparison_meeting = club.meetings[-2]
+
+    last_meeting.attendee_count_percentage_delta(comparison_meeting)
+  end
+
+  def club_meetings_average_rating_percentage_delta(club)
+    last_meeting = club.meetings[-1]
+    comparison_meeting = club.meetings[-2]
+
+    last_meeting.average_rating_percentage_delta(comparison_meeting)
   end
 end
--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -2,8 +2,27 @@ class Meeting < ActiveRecord::Base
   belongs_to :club
   has_many :feedback_responses, dependent: :destroy
 
+  def attendee_count
+    self.feedback_responses.count
+  end
+
   def average_rating
     ratings = self.feedback_responses.map { |resp| resp.rating }
     ratings.inject { |sum, el| sum + el }.to_f / ratings.size
+  end
+
+  def attendee_count_percentage_delta(meeting)
+    percentage_delta(self.attendee_count, meeting.attendee_count)
+  end
+
+  def average_rating_percentage_delta(meeting)
+    percentage_delta(self.average_rating, meeting.average_rating)
+  end
+
+  private
+
+  def percentage_delta(c1, c2)
+    delta = c1 - c2
+    delta.to_f / c1 * 100
   end
 end
