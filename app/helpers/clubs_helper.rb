6808fe21015c7a364086012d74cf6954ed075242
Zach Latta <zach@zachlatta.com>
2015-09-24 13:00:05 -0700

    Make charts work

--- app/helpers/clubs_helper.rb
+++ app/helpers/clubs_helper.rb
@@ -1,2 +1,39 @@
 module ClubsHelper
+  def club_last_feedback_count(club)
+    club.last_feedback_count || 'N/A'
+  end
+
+  def club_last_meeting_date(club)
+    date = club.last_meeting_date
+
+    date.nil? ? 'N/A' : date.strftime('%m/%d/%Y')
+  end
+
+  def club_last_average_rating(club)
+    meeting = club.meetings.last
+
+    meeting.nil? ? 'N/A' : meeting.average_rating
+  end
+
+  def club_meetings_chart_labels(club)
+    [
+      data: club.meetings.map { |m| m.id }
+    ]
+  end
+
+  def club_meetings_attendees_chart_data(club)
+    [
+      {
+        data: club.meetings.map { |m| m.feedback_responses.count }
+      }
+    ]
+  end
+
+  def club_meetings_rating_chart_data(club)
+    [
+      {
+        data: club.meetings.map { |m| m.average_rating }
+      }
+    ]
+  end
 end
--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,4 +1,9 @@
 class Meeting < ActiveRecord::Base
   belongs_to :club
   has_many :feedback_responses, dependent: :destroy
+
+  def average_rating
+    ratings = self.feedback_responses.map { |resp| resp.rating }
+    ratings.inject { |sum, el| sum + el }.to_f / ratings.size
+  end
 end
