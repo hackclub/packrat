d3fc484baabce96bf75642df858f5720800a3995
Zach Latta <zach@zachlatta.com>
2015-09-30 12:36:03 -0700

    Fix password removal migration

--- db/migrate/20150929231551_remove_columns_from_club_members.rb
+++ db/migrate/20150929231551_remove_columns_from_club_members.rb
@@ -1,6 +1,5 @@
 class RemoveColumnsFromClubMembers < ActiveRecord::Migration
   def change
-    remove_column :club_members, :password, :string
     remove_column :club_members, :encrypted_password, :string
     remove_column :club_members, :reset_password_token, :string
     remove_column :club_members, :reset_password_sent_at, :string
